ca1bcae087c256d11e3148fc46806a030cf7fe6a
souzajulen-ui <souzajulen@gmail.com>
2026-08-04 22:23:25 -0300

    Atualiza vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,13 @@
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-  vite: {
+export default defineConfig(({ mode }) => ({
+    server: { 
+      host: "::",
+      port: 8080,
+    },
     base: "/",
-  },
+
   tanstackStart: {
-    // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
-    // nitro/vite builds from this
-    server: { entry: "server" },
   },
 });
